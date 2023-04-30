b0f6aa9c928ce2c95562022d3934eb72b59b4165
YangLorenzo <yangrongwei2002@gmail.com>
2023-04-30 17:03:21 +0200

    completed modal window

--- js/anime-modal.js
+++ js/anime-modal.js
@@ -3,18 +3,42 @@ import * as AnimeAlbum from "./modules/anime-album.js";
 AnimeAlbum.setModalWindowHandler(displayAnimeModal);
 
 function displayAnimeModal(anime) {
+    // title
     const modalTitle = document.querySelector("#modal-title");
-    const modalImage = document.querySelector("#modal-image");
-    const modalDetails = document.querySelector("#modal-details");
-
     modalTitle.textContent = anime.title;
 
+    // image
+    const modalImage = document.querySelector("#modal-image");
     modalImage.src = anime.images.jpg.image_url;
     modalImage.alt = anime.title;
 
-    modalDetails.textContent = anime.synopsis;
+    // synopsis
+    const modalSynopsis = document.querySelector("#modal-synopsis");
+    modalSynopsis.textContent = anime.synopsis;
 
-    const modal = new bootstrap.Modal(document.getElementById("anime-modal"), {
+    // genres
+    const modalGenres = document.querySelector("#modal-genres");
+    anime.genres
+        .map((genre) => genre.name)
+        .forEach((genre) => {
+            const newGenre = document.createElement("li");
+            newGenre.textContent = genre;
+            modalGenres.appendChild(newGenre);
+        });
+
+    // rank
+    const modalRank = document.querySelector("#modal-rank");
+    modalRank.textContent = anime.rank;
+
+    // score
+    const modalScore = document.querySelector("#modal-score");
+    modalScore.textContent = anime.score;
+
+    // rating
+    const modalRating = document.querySelector("#modal-rating");
+    modalRating.textContent = anime.rating;
+
+    const modal = new bootstrap.Modal(document.querySelector("#anime-modal"), {
         keyboard: true,
         focus: true,
     });
