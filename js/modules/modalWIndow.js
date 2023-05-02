5cb3cc49f776b15e6a4ff7c147409ee38b19b399
YangLorenzo <yangrongwei2002@gmail.com>
2023-05-02 13:53:07 +0200

    refactored and added doc: modalWindow.js, quote.js

---
+++ js/modules/modalWIndow.js
@@ -0,0 +1,60 @@
+/**
+ * Module for providing functionality for displaying modal windows
+ * @module ModalWindow
+ */
+
+/**
+ * Displays the anime modal with information about the anime
+ *
+ * @param {Object} anime - The anime object containing information to display
+ */
+function displayAnimeModalCallback(anime) {
+    // title
+    const modalTitle = document.querySelector("#modal-title");
+    modalTitle.textContent = anime.title;
+
+    // image
+    const modalImage = document.querySelector("#modal-image");
+    modalImage.src = anime.images.jpg.image_url;
+    modalImage.alt = anime.title;
+
+    // synopsis
+    const modalSynopsis = document.querySelector("#modal-synopsis");
+    modalSynopsis.textContent = anime.synopsis;
+
+    // genres
+    const modalGenres = document.querySelector("#modal-genres");
+    anime.genres
+        .map((genre) => genre.name)
+        .forEach((genre) => {
+            const newGenre = document.createElement("li");
+            newGenre.textContent = genre;
+            modalGenres.appendChild(newGenre);
+        });
+
+    // rank
+    const modalRank = document.querySelector("#modal-rank");
+    modalRank.textContent = anime.rank;
+
+    // score
+    const modalScore = document.querySelector("#modal-score");
+    modalScore.textContent = anime.score;
+
+    // rating
+    const modalRating = document.querySelector("#modal-rating");
+    modalRating.textContent = anime.rating;
+
+    const modal = new bootstrap.Modal(document.querySelector("#anime-modal"), {
+        keyboard: true,
+        focus: true,
+    });
+
+    modal._element.addEventListener("hidden.bs.modal", () => {
+        const modalGenres = document.querySelector("#modal-genres");
+        modalGenres.innerHTML = "";
+    });
+
+    modal.show();
+}
+
+export { displayAnimeModalCallback };
